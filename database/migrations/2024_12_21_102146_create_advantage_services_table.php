c1130d4be02da8a7f62dc01be861702bc41b8650
Royhan <royhankeren12@gmail.com>
2024-12-21 17:28:26 +0700

    update advantage services table

--- database/migrations/2024_12_21_102146_create_advantage_services_table.php
+++ database/migrations/2024_12_21_102146_create_advantage_services_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('advantage_services', function (Blueprint $table) {
             $table->id();
+            $table->integer('services_id')->nullable();
+            $table->string('advantage');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
