75be19b18ced1568140e2eb772871c7fcc166ed9
Royhan <royhankeren12@gmail.com>
2024-12-26 14:36:33 +0700

    Exprience User && Detail User models

--- app/Models/ExprienceUser.php
+++ app/Models/ExprienceUser.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ExprienceUser extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
+
+    use SoftDeletes;
+
+    public $table = 'exprience_user';
+    protected $dates = [
+        'updated_at',
+        'created_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'detail_user_id',
+        'exprience',
+        'updated_at',
+        'created_at',
+        'deleted_at',
+    ];
 }
