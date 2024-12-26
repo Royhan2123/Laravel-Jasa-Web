c97bb72636ee64f0ad2096bb5694aa0b8403a10e
Royhan <royhankeren12@gmail.com>
2024-12-26 14:59:00 +0700

    order_status --update

--- app/Models/OrderStatus.php
+++ app/Models/OrderStatus.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class OrderStatus extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
+
+    public $table = 'order_status';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'name',
+    ];
 }
